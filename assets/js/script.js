eecc26743c80764f2018bf212f04306ee2cfe322
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-07-11 22:20:02 -0400

    documentacion del codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+//Evento tooltip para botón de envío formulario
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+//Evento de scroll que modifica la barra de navegación
 const nav = document.querySelector('.navbar');
 
 window.addEventListener('scroll', function(){
